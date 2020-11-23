b4b6a9e2584381025a0e87b99a4e3d4571f4575e
zhaoys <zhaoys@centaline.com.cn>
2020-11-23 11:01:03 +0800

    init

---
+++ publish-plugin/src/main/groovy/publish/plugin/CommandTask.kt
@@ -0,0 +1,22 @@
+package com.zys.publish.rename
+
+import org.gradle.api.tasks.AbstractExecTask
+import org.gradle.internal.os.OperatingSystem
+
+/**
+ * 描述:
+ *
+ * author zys
+ * create by 2020/11/19
+ */
+open class CommandTask : AbstractExecTask<CommandTask>(CommandTask::class.java) {
+    override fun commandLine(vararg arguments: Any?): CommandTask {
+        val windows = OperatingSystem.current().isWindows
+        if (windows) {
+            commandLine("cmd", "/c", "gradlew.bat", arguments)
+        } else {
+            commandLine("gradlew", arguments)
+        }
+        return this
+    }
+}
---
+++ publish-plugin/src/main/groovy/publish/plugin/PluginConst.kt
@@ -0,0 +1,12 @@
+package publish.plugin
+
+/**
+ * 描述:
+ *
+ * author zys
+ * create by 2020/11/19
+ */
+@Suppress("SpellCheckingInspection")
+object PluginConst {
+    const val GROUP = "centa"
+}
